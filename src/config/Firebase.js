1bc7876767bef265fe0b5093cce298337b9a5cb4
Bruno DaSilva <hi.dasilvab@gmail.com>
2020-05-26 11:21:10 -0500

    ADDED ENVIROMENT VARIABLES

--- src/config/Firebase.js
+++ src/config/Firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 // Your web app's Firebase configuration
 const config = {
-  apiKey: "AIzaSyARUwcw7ICMzzGtLe1W9XgIjj_q7m2WvUc",
-  authDomain: "react-login-form-2db9a.firebaseapp.com",
-  databaseURL: "https://react-login-form-2db9a.firebaseio.com",
-  projectId: "react-login-form-2db9a",
-  storageBucket: "react-login-form-2db9a.appspot.com",
-  messagingSenderId: "1074936303210",
-  appId: "1:1074936303210:web:59884aa4cdc11dd230c413",
-  measurementId: "G-V9SV4HXTYS"
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
+  measurementId: REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const fire = firebase.initializeApp(config);
