1cfe61342a57cb3ab8ba4e1e4c4a6233530a3475
Bruno DaSilva <hi.dasilvab@gmail.com>
2020-05-26 11:24:28 -0500

    Fixed env variables on firebase.js

--- src/config/Firebase.js
+++ src/config/Firebase.js
@@ -7,8 +7,8 @@ const config = {
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-  appId: REACT_APP_FIREBASE_APP_ID,
-  measurementId: REACT_APP_FIREBASE_MEASUREMENT_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const fire = firebase.initializeApp(config);
